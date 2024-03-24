86b0386bf0dfd1cdcb07b49bf0203859155d7845
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-03-24 10:54:40 +0200

    solution

---
+++ 01.Syntax-Fundamentals/Exercise/06.SumDigits/06.SumDigits.js
@@ -0,0 +1,10 @@
+function sumDigits(number){
+    let sum = 0;
+    
+    while (number > 0) {
+        sum += number % 10;
+
+        number = Math.floor(number / 10);
+    }
+    console.log(sum)
+}
